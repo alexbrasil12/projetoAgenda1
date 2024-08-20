86f0c63decb9fbbc726327d64712083c59dd1eb9
alexander.brasil <alexander.brasil@dcx.ufpb.br>
2024-08-20 09:40:24 -0300

    terceira mundaça

--- src/main/java/br/ufpb/dcx/ayla/agenda/AgendaAyla.java
+++ src/main/java/br/ufpb/dcx/ayla/agenda/AgendaAyla.java
@@ -1,7 +1,16 @@
 package br.ufpb.dcx.ayla.agenda;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class AgendaAyla implements Agenda{
     private Map<String,Contato> contatos;
+    private GravadorDeDados gravador;
+
+    public AgendaAyla(){
+        this.contatos = new HashMap<>();
+        this.gravador = new GravadorDeDados();
+    }
+
+    public boolean cadastraContato(String nome, int )
 }
